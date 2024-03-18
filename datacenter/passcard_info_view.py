639b44947d554c6445249812c9392550b2998b00
fergoth <fergothew@gmail.com>
2024-03-18 13:21:07 +0300

    Реализация просмотра всех посещений владельца пасскарты

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,18 +1,18 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = get_object_or_404(Passcard,passcode=passcode)
+    current_passcard_visits = Visit.objects.filter(passcard__passcode=passcode)
 
     this_passcard_visits = [
         {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
+            'entered_at': visit.entered_at,
+            'duration': visit.format_duration(),
+            'is_strange': visit.is_long()
+        } for visit in current_passcard_visits
     ]
     context = {
         'passcard': passcard,
