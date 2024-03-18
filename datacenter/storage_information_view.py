baf57aea239c74ff1ecdb22d7280175a10d1f936
fergoth <fergothew@gmail.com>
2024-03-18 13:20:25 +0300

    Реализация просмотра незакрытых посещений

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,16 +2,17 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
+from django.utils.timezone import localtime
 
 def storage_information_view(request):
-    # Программируем здесь
-
+    visits = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = [
         {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': localtime(visit.entered_at),
+            'duration': visit.format_duration()
         }
+        for visit in visits
     ]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
